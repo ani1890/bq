edca1932b0314145b44abe4fa889790fc2c23b1c
ani1890 <142164897+ani1890@users.noreply.github.com>
2025-04-28 22:55:06 +0530

    Update bq.py

--- bq.py
+++ bq.py
@@ -4,11 +4,3 @@ import pandas as pd
 
 # Title
 st.title("BigQuery + Streamlit Demo")
-
-# Set your GCP project ID
-project_id = "ab-dev-410613"
-
-# Create BigQuery client
-client = bigquery.Client(project=project_id)
-
-st.write(client)
